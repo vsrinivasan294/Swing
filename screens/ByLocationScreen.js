eebdfead1115e13cad730188c561da3917d77bc4
Vishnu Srinivasan <vishnunaga_srinivasan@gmail.com>
2019-11-20 16:46:05 -0600

    New features

--- screens/ByLocationScreen.js
+++ screens/ByLocationScreen.js
@@ -1,12 +1,197 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Animated,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  Navigator,
+} from "react-native";
 
-export default class App extends React.Component {
+import MapView from "react-native-maps";
+
+const Images = [
+  { uri: "https://i.imgur.com/iwyuxOR.jpg" },
+  { uri: "https://i.imgur.com/aUeBiR8.jpg" },
+  { uri: "https://i.imgur.com/4qm8LWE.jpg" },
+  { uri: "https://i.imgur.com/TJzHJcZ.jpg" }
+]
+
+const { width, height } = Dimensions.get("window");
+
+const CARD_HEIGHT = height / 4;
+const CARD_WIDTH = CARD_HEIGHT - 50;
+
+export default class screens extends Component {
+  state = {
+    markers: [
+      {
+        coordinate: {
+          latitude: 45.524548,
+          longitude: -122.6749817,
+        },
+        title: "Hammock Beach",
+        description: "This is the best Hammock on the beach",
+        image: Images[0],
+      },
+      {
+        coordinate: {
+          latitude: 45.524698,
+          longitude: -122.6655507,
+        },
+        title: "Hammock Mountain",
+        description: "This is the best Hammock on the mountain",
+        image: Images[1],
+      },
+      {
+        coordinate: {
+          latitude: 45.5230786,
+          longitude: -122.6701034,
+        },
+        title: "Hammock Forest",
+        description: "This is the best Hammock on the forest",
+        image: Images[2],
+      },
+      {
+        coordinate: {
+          latitude: 45.521016,
+          longitude: -122.6561917,
+        },
+        title: "Hammock Nature",
+        description: "This is the best Hammock in nature",
+        image: Images[3],
+      },
+    ],
+    region: {
+      latitude: 45.52220671242907,
+      longitude: -122.6653281029795,
+      latitudeDelta: 0.04864195044303443,
+      longitudeDelta: 0.040142817690068,
+    },
+  };
+
+  componentWillMount() {
+    this.index = 0;
+    this.animation = new Animated.Value(0);
+  }
+  componentDidMount() {
+    // We should detect when scrolling has stopped then animate
+    // We should just debounce the event listener here
+    this.animation.addListener(({ value }) => {
+      let index = Math.floor(value / CARD_WIDTH + 0.3); // animate 30% away from landing on the next item
+      if (index >= this.state.markers.length) {
+        index = this.state.markers.length - 1;
+      }
+      if (index <= 0) {
+        index = 0;
+      }
+
+      clearTimeout(this.regionTimeout);
+      this.regionTimeout = setTimeout(() => {
+        if (this.index !== index) {
+          this.index = index;
+          const { coordinate } = this.state.markers[index];
+          this.map.animateToRegion(
+            {
+              ...coordinate,
+              latitudeDelta: this.state.region.latitudeDelta,
+              longitudeDelta: this.state.region.longitudeDelta,
+            },
+            350
+          );
+        }
+      }, 10);
+    });
+  }
+
   render() {
+    const interpolations = this.state.markers.map((marker, index) => {
+      const inputRange = [
+        (index - 1) * CARD_WIDTH,
+        index * CARD_WIDTH,
+        ((index + 1) * CARD_WIDTH),
+      ];
+      const scale = this.animation.interpolate({
+        inputRange,
+        outputRange: [1, 2.5, 1],
+        extrapolate: "clamp",
+      });
+      const opacity = this.animation.interpolate({
+        inputRange,
+        outputRange: [0.35, 1, 0.35],
+        extrapolate: "clamp",
+      });
+      return { scale, opacity };
+    });
+
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle} />
+        <MapView
+          ref={map => this.map = map}
+          initialRegion={this.state.region}
+          style={styles.container}
+        >
+          {this.state.markers.map((marker, index) => {
+            const scaleStyle = {
+              transform: [
+                {
+                  scale: interpolations[index].scale,
+                },
+              ],
+            };
+            const opacityStyle = {
+              opacity: interpolations[index].opacity,
+            };
+            return (
+              <MapView.Marker key={index} coordinate={marker.coordinate}>
+                <Animated.View style={[styles.markerWrap, opacityStyle]}>
+                  <Animated.View style={[styles.ring, scaleStyle]} />
+                  <View style={styles.marker} />
+                </Animated.View>
+              </MapView.Marker>
+            );
+          })}
+        </MapView>
+        <Animated.ScrollView
+          horizontal
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: this.animation,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          style={styles.scrollView}
+          contentContainerStyle={styles.endPadding}
+        >
+          {this.state.markers.map((marker, index) => (
+            <View style={styles.card} key={index}>
+              <Image
+                source={marker.image}
+                style={styles.cardImage}
+                resizeMode="cover"
+              />
+              <View style={styles.textContent}>
+                <Text numberOfLines={1} style={styles.cardtitle}>{marker.title}</Text>
+                <Text numberOfLines={1} style={styles.cardDescription}>
+                  {marker.description}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </Animated.ScrollView>
       </View>
     );
   }
@@ -15,12 +200,67 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+  scrollView: {
+    position: "absolute",
+    bottom: 30,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  textContent: {
+    flex: 1,
+  },
+  cardtitle: {
+    fontSize: 12,
+    marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marker: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "rgba(130,4,150, 0.9)",
+  },
+  ring: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: "rgba(130,4,150, 0.3)",
+    position: "absolute",
+    borderWidth: 1,
+    borderColor: "rgba(130,4,150, 0.5)",
   },
 });
+
+AppRegistry.registerComponent("mapfocus", () => screens);
--- screens/BySpotScreen.js
+++ screens/BySpotScreen.js
@@ -1,30 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Navigator, TextInput, Alert, AppRegistry, FlatList } from 'react-native';
-import logo from './assets/images/logo.jpg';
-import styles from './assets/css/styles';
+import {StyleSheet, Text, View, Slider} from 'react-native';
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 50,
+    };
+  }
 
-export default class ByProductScreen extends React.Component {
+  change(value) {
+    this.setState(() => {
+      return {
+        value: parseFloat(value),
+
+      };
+    });
+  }
+
   render() {
+    const {value} = this.state;
     return (
-    <View style={styles.container}>
-        <View style={styles.topContainer}>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('BySpot')}>Illini Union</Text>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('ByLocation')}>Altgeld Hall</Text>
-           <Text style={styles.h3} onPress={() => this.props.navigation.navigate('BySpot')}>Illini Union</Text>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('ByLocation')}>Siebel Center</Text>
-           <Text style={styles.h3} onPress={() => this.props.navigation.navigate('BySpot')}>ECEB</Text>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('ByLocation')}>South Quad</Text>
-           <Text style={styles.h3} onPress={() => this.props.navigation.navigate('BySpot')}>Main Quad</Text>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('ByLocation')}>Bardeen Quad</Text>
-           <Text style={styles.h3} onPress={() => this.props.navigation.navigate('BySpot')}>Ikenberry Commons</Text>
-          <Text style={styles.h3} onPress={() => this.props.navigation.navigate('ByLocation')}>BIF</Text>
-         
-        </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>Radius</Text>
+        <Slider
+          step={1}
+          maximumValue={100}
+          onValueChange={this.change.bind(this)}
+          value={value}
+        />
+
+        <Text style={styles.text}>Capacity</Text>
+        <Slider
+          step={1}
+          maximumValue={10}
+          onValueChange={this.change.bind(this)}
+          value={value}
+        />
+        
       </View>
-     
     );
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50,
+    textAlign: 'center',
+  },
+});
